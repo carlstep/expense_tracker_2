c91bf6749105c92f96ccf95ab090d395205e9888
carlstep <carlstep333@me.com>
2023-06-22 07:08:28 +0700

    created onRemoveExpense

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -8,15 +8,27 @@ class ExpensesList extends StatelessWidget {
   const ExpensesList({
     Key? key,
     required this.expenses,
+    required this.onRemoveExpense,
   }) : super(key: key);
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(
+          expenses[index],
+        ),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(
+          expense: expenses[index],
+        ),
+      ),
     );
   }
 }
